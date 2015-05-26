9bef429e4d0104a73b5e56c51966965f5c980e02
Leandro Guillen <leandro.guillen@imdea.org>
2015-05-26 11:16:45 +0200

    Small change

--- ngsi/ngsi.go
+++ ngsi/ngsi.go
@@ -5,16 +5,18 @@ import (
 	"github.com/codegangsta/cli"
 	"os"
 	"strconv"
+	"fmt"
 )
 
 var configFile = "/tmp/ngsi.config"
+var g gongsi.Gongsi
 
 func main() {
 
 	// Set up Gongsi
-	g := gongsi.Gongsi{
+	g = gongsi.Gongsi{
 		Encoding: "application/json",
-		Host:     "leandroguillen.com",
+		Host:     "localhost",
 		Port:     1026,
 	}
 	g.Init()
@@ -28,8 +30,8 @@ func main() {
 
 	// Default operation with no parameters
 	app.Action = func(c *cli.Context) {
-		println("USAGE: ngsi [global options] command [command options] [arguments...]")
-		println("Type 'ngsi help' for help")
+		fmt.Println("USAGE: ngsi [global options] command [command options] [arguments...]")
+		fmt.Println("Type 'ngsi help' for help")
 	}
 
 	app.Commands = []cli.Command{
@@ -37,24 +39,7 @@ func main() {
 			Name:      "query",
 			ShortName: "q",
 			Usage:     "Query operation",
-			Action: func(c *cli.Context) {
-				
-				arg1 := c.Args().Get(0)
-				arg2 := c.Args().Get(1)
-
-				if arg1 != "" && arg2 != "" {
-					// Query for an attribute
-					out, _ := g.ConvQueryContextAttribute(arg1, arg2)
-					println(out)
-				} else if arg1 != "" {
-					// Query for an entity
-					out, _ := g.ConvQueryContext(arg1)
-					println(out)
-				} else {
-					println("You must provide an ID:")
-					println("   ngsi get <ID>")
-				}							
-			},
+			Action:    query,
 		},
 		{
 			Name:      "update",
@@ -75,37 +60,65 @@ func main() {
 		{
 			Name:      "host",
 			Usage:     "Set up context broker host",
-			Action: func(c *cli.Context) {
-				
-				arg1 := c.Args().Get(0)
-				arg2 := c.Args().Get(1)
-
-				if arg1 != "" {
-					
-					// Setting up the host
-					println("setting up host")
-					if _, err := os.Stat(configFile); err == nil {
-					    println(configFile + " exists; processing...")
-					} else {
-						println("file does not exist")
-					}
-						
-				} else if arg1 != "" && arg2 != "" {
-					// Setting up the host and the port
-					println("setting up host and port")
-
-				} else {
-					// Get info
-					println("host: " + g.Host)
-					println("port: " + strconv.Itoa(g.Port))
-					
-
-				}
-					
-			},
+			Action:    host,
 		},
 	}
 
 	app.Run(os.Args)
 
+}
+
+func query(c *cli.Context) {
+	arg1 := c.Args().Get(0)
+	arg2 := c.Args().Get(1)
+
+	if arg1 != "" && arg2 != "" {
+		// Query for an attribute
+		out, err := g.ConvQueryContextAttribute(arg1, arg2)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(out)
+		}
+	} else if arg1 != "" {
+		// Query for an entity
+		out, err := g.ConvQueryContext(arg1)
+		if err != nil {
+			fmt.Println(err)	
+		} else {
+			fmt.Println(out)
+		}
+	} else {
+		fmt.Println("You must provide an ID:")
+		fmt.Println("   ngsi get <ID>")
+	}		
+}
+
+func host(c *cli.Context) {				
+	arg1 := c.Args().Get(0)
+	arg2 := c.Args().Get(1)
+
+	if arg1 != "" {
+		
+		// Setting up the host
+		fmt.Println("setting up host")
+		if _, err := os.Stat(configFile); err == nil {
+		    fmt.Println(configFile + " exists; processing...")
+		} else {
+			fmt.Println("file does not exist")
+		}
+			
+	} else if arg1 != "" && arg2 != "" {
+		// Setting up the host and the port
+		fmt.Println("setting up host and port")
+
+	} else {
+		// Get info
+		fmt.Println("Current settings:")
+		fmt.Println("  host: " + g.Host)
+		fmt.Println("  port: " + strconv.Itoa(g.Port))
+		
+
+	}
+		
 }
